d3e0396d2b90970291b480657144ed36b08adcf8
Lohit Muralidharan <lohit308@gmail.com>
2022-09-05 15:58:32 -0500

    Deploy App pls

--- NotesApp/settings.py
+++ NotesApp/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-zq-aw@l_^fv1=k2)m%g7r55ilgfz1p1mhkajb=!35)w2%3&r)m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -120,7 +120,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = [
     BASE_DIR/ 'staticfiles'
 ]
