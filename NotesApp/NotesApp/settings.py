d703bf24ca1557325ea935dfedccf45e73ef9be2
LohitoBurrito <110351173+LohitoBurrito@users.noreply.github.com>
2022-12-11 05:08:26 -0600

    Update settings.py

--- NotesApp/NotesApp/settings.py
+++ NotesApp/NotesApp/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-zq-aw@l_^fv1=k2)m%g7r55ilgfz1p1mhkajb=!35)w2%3&r)m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
